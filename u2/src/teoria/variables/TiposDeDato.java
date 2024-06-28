8df21d86f7d5129c978079c5a1821ea60780eac9
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2024-06-28 18:53:00 -0300

    teoria (u2): Tipos de dato, declaración e inicialización de variables

---
+++ u2/src/teoria/variables/TiposDeDato.java
@@ -0,0 +1,17 @@
+package teoria.variables;
+
+public class TiposDeDato {
+    public static void main(String[] args) {
+
+        int miEntero = 10;
+        double miFlotante;
+        char miCaracter;
+        boolean miBoolean;
+        String miCadena;
+
+        miFlotante = 20.5;
+        miCaracter = 'a';
+        miBoolean = false;
+        miCadena = "chau mundo";
+    }
+}
