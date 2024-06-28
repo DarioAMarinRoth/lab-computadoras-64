580ed4a35d5341c3a2ebf1fd4c47d95484b5af0e
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2024-06-28 19:10:35 -0300

    teoria (u2): Entrada estándar (Objetos Scanner)

---
+++ u2/src/teoria/variables/EntradaEstandar.java
@@ -0,0 +1,16 @@
+package teoria.variables;
+import java.util.Scanner;
+
+public class EntradaEstandar {
+    public static void main(String[] args) {
+
+        int miEntero;
+        double miFlotante;
+        String miTexto;
+        Scanner teclado = new Scanner(System.in);
+
+        miEntero = teclado.nextInt();           // Leemos un entero
+        miFlotante = teclado.nextDouble();      // Leemos un flotante
+        miTexto = teclado.nextLine();           // Leemos texto
+    }
+}
