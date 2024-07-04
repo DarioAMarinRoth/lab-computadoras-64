bc98592b12b101fabde2f42724ed8507d2eb89d0
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2024-07-04 01:00:18 -0300

    Teoría (u2): vemos la estructura while

---
+++ u2/src/teoria/estructuras/Iteraciones.java
@@ -0,0 +1,18 @@
+package teoria.estructuras;
+
+public class Iteraciones {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 15;
+
+        System.out.println("Antes del while");
+
+        while (a < 10) {
+            System.out.println("Hola mundo");
+            a++;
+        }
+        System.out.println("Terminó el loop");
+
+    }
+}
