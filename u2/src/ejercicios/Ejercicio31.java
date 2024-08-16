f31beeed0f357e9530aed10468b5fa79fef128ce
Dario Alejandro Marin Roth <dariomarinroth@gmail.com>
2024-08-16 18:51:29 -0300

    Resolvemos el ejercicio 31 y lo probamos

---
+++ u2/src/ejercicios/Ejercicio31.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+import static teoria.funciones.Funciones.raizCuadrada;
+
+public class Ejercicio31 {
+    public static void main(String[] args) {
+        System.out.printf("%.5f", raizCuadrada(12));
+
+        // print()
+        // println()
+        // printf()
+
+
+    }
+}
--- u2/src/teoria/funciones/Funciones.java
+++ u2/src/teoria/funciones/Funciones.java
@@ -2,17 +2,40 @@ package teoria.funciones;
 
 public class Funciones {
 
-    int numeroIngresado = 1;
 
     public static void saludar(String nombreSaludado) {
         System.out.println("Esta es la función saludar.");
         System.out.println("Hola " + nombreSaludado + "!");
     }
 
-    public static boolean esPar(int numero){
+    public static boolean esPar(int numero) {
         boolean esPar;
         esPar = numero % 2 == 0;
         return esPar;
+    }
+
+    public int sumar(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static boolean sonIguales(double num1, double num2, double tolerancia) {
+        return (num1 - num2 <= tolerancia) && (num1 - num2 >= -1 * tolerancia);
+    }
+
+    public static double raizCuadrada(double argumento) {
+
+        double tolerancia = 0.00001;
+        double area = argumento;
+        double base = area;
+        double altura = 1;
+
+        while (!sonIguales(base, altura, tolerancia)) {
+            base = (base + altura) / 2; // Calculamos un nuevo valor para la base, tomando el promedio de los lados.
+            altura = area / base;
+        }
+
+        return base;
+
     }
 
 }
