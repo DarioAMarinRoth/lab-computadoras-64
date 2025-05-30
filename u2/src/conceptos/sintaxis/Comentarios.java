b9f0652262c00262bdace4fe080fc51f1fc32edc
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-05-30 19:40:02 -0300

    explicación sobre los comentarios

---
+++ u2/src/conceptos/sintaxis/Comentarios.java
@@ -0,0 +1,27 @@
+package conceptos.sintaxis;
+
+public class Comentarios {
+
+    // Esto es un comentario de una única línea. Shortcut: Ctrl + /
+
+    /*
+    Muchas
+    Líneas
+    comentadas
+    Shortcut: Ctrl + Shift + /
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Cómo"); // Todo: Borrar el tilde de la ó.
+        System.out.println("Estás?"); // Todo: cambiar la mayúscula
+    }
+
+    // Comentarios facheros -> plugin: Better Highlights
+
+    // ! Comentario de error
+    // * Comentario de nota/información
+    // ? Comentario de pregunta
+
+
+}
