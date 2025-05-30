cedd3a6b861f212dab630d9330a87deb60f98616
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-05-30 19:43:17 -0300

    propuesta de ejercicio para practicar declaración e inicialización de variables y utilización de la salida estándar

---
+++ u2/src/ejercicios/EjercicioInicial.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class EjercicioInicial {
+    /* Escriba un programa en Java donde se declaren las variables:
+    - nombre
+    - edad
+    - altura
+    Inicialice cada una de las variables con su propio nombre, edad y
+    altura. Para finalizar, Imprima todos los datos juntos en una misma
+    cadena de texto.
+    */
+}
