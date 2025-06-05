27802187123fcf0c8afe547fd0a2041549b3f751
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-04 22:08:32 -0300

    proponemos un ejercicio para resolver utilizando Scanner

--- u2/src/ejercicios/EjercicioEntradas.java
+++ u2/src/ejercicios/EjercicioEntradas.java
@@ -1,4 +1,11 @@
 package ejercicios;
 
 public class EjercicioEntradas {
+    /*
+    Diseñe un programa que permita cargar: la altura, la edad y el nombre del usuario
+    exactamente en ese orden, y que luego imprima toda la información del usuario en la
+    consola.
+     */
+
+    // Import para el scanner: import java.util.Scanner;
 }
