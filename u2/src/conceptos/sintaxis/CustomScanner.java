357aac90d8ab658fcba54426247dd37ee99efbdb
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-04 22:04:51 -0300

    vemos como funciona un objeto Scanner

--- u2/src/conceptos/sintaxis/CustomScanner.java
+++ u2/src/conceptos/sintaxis/CustomScanner.java
@@ -1,4 +1,18 @@
 package conceptos.sintaxis;
 
-public class Scanner {
+import java.util.Scanner;
+
+public class CustomScanner {
+    public static void main(String[] args) {
+
+        String cuentito = "3 tristes tigres \n tragaban trigo \n en un trigal";
+        Scanner miScanner = new Scanner(cuentito);
+        String linea = miScanner.nextLine();
+        System.out.println(linea);
+        linea = miScanner.nextLine();
+        System.out.println(linea);
+        linea = miScanner.nextLine();
+        System.out.println(linea);
+        miScanner.close();
+    }
 }
