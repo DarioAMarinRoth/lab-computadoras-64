2ca7e11d01ecce2b818900cea9e7cdf364189398
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-13 19:09:12 -0300

    vemos como implementar un loop, además de instrucciones break, continue y operadores de incremento

--- u2/src/conceptos/control/Iteracion.java
+++ u2/src/conceptos/control/Iteracion.java
@@ -1,4 +1,20 @@
 package conceptos.control;
 
 public class Iteracion {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+
+        System.out.println("El comienzo del programa");
+        while (num < 10) {
+            num++; // num++ ó ++num Peeeeeeeeero num++ != ++num
+            if (num == 5 || num == 7) {
+                continue;
+            }
+            System.out.println("Estoy adentro del loop. Voy por la iteración N° " + num);
+        }
+        System.out.println("Terminó el programa");
+
+    }
 }
