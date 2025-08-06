b877e412af7392c503ac7f0d2ecc2e9a1e352f29
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-08-06 20:48:02 -0300

    clase de Strings

--- u2/src/conceptos/strings/CadenasDeTexto.java
+++ u2/src/conceptos/strings/CadenasDeTexto.java
@@ -1,4 +1,11 @@
 package conceptos.strings;
 
 public class CadenasDeTexto {
+    public static void main(String[] args) {
+        String miString = "Hola mundo";
+        int i = 10;
+        System.out.println(miString.charAt(i));
+
+
+    }
 }
--- u2/src/ejercicios/Ejercicio30.java
+++ u2/src/ejercicios/Ejercicio30.java
@@ -1,4 +1,25 @@
+
 package ejercicios;
 
+import static utilidades.Funciones.potencia;
+
 public class Ejercicio30 {
+
+    public static void main(String[] args) {
+        System.out.println(deBinarioADecimal(1001010111));
+    }
+
+    public static int deBinarioADecimal(int binario) {
+        int decimal = 0;
+        int base = 10;
+        int i = 0;
+
+        while (binario != 0) {
+            int digito = binario % base;
+            binario = binario / base;
+            decimal += (int) (digito * potencia(2, i));
+            i++;
+        }
+        return decimal;
+    }
 }
