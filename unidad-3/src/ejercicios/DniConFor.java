0f8daf23cb930e7ca1212cfe524f4fab68fe2a4e
Dario A. Marin Roth <dariomarinroth@gmail.com>
2026-08-19 21:34:09 -0300

    hacemos una función para imprimir vectores

--- unidad-3/src/ejercicios/DniConFor.java
+++ unidad-3/src/ejercicios/DniConFor.java
@@ -1,16 +1,33 @@
 void main() {
 
-    int[] dni = new int[8];
+    int[] dni1 = {4, 0, 0, 0, 0, 0, 0, 0};
+    int[] dni2 = {4, 0, 0, 0, 0, 0, 0, 1};
+    int[] dni3 = {4, 0, 0, 0, 0, 0, 0, 2};
+    int[] vectorPrueba = {1, 2, 3};
+    int[] ficticio = {1, 1, 1, 1, 1, 1};
 
 
-    for (int i = 0; i < 8; i++) {
-        dni[i] = Integer.parseInt(IO.readln());
-    }
 
-    IO.println("El dni ingresado es: ");
+//    IO.println("El dni ingresado es: ");
+//
 
-    for (int i = 0; i < 8; i++) {
-        IO.println(dni[i]);
-    }
+    IO.println("El vector prueba tiene " + ficticio.length +" elementos" );
+
+    imprimirVector(dni1);
+    imprimirVector(vectorPrueba);
+    imprimirVector(ficticio);
 
 }
+
+void imprimirVector(int[] vector) {
+
+    for (int coordenada = 0; coordenada < vector.length; coordenada++) {
+        IO.print(vector[coordenada]);
+    }
+    IO.println();
+}
+
+// Dejar que el usuario cargue un vector de n elementos. Luego calcular e imprimir la suma de
+// todos los elementos.
+
+// Realizar una función que reciba un vector de enteros y que DEVUELVA la suma de sus elementos.
