35b55c33fd3b1ad229bd7a72f5e5fa7be1ea2bd1
Dario A. Marin Roth <dariomarinroth@gmail.com>
2026-09-23 19:44:50 -0300

    arrancamos la unidad 4

---
+++ unidad-4/src/clases/Contacto.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class Contacto {
+    public String nombre;
+    public String apellido;
+    public String numeroCelular;
+    public String email;
+}
---
+++ unidad-4/src/clases/Main.java
@@ -0,0 +1,31 @@
+import clases.Contacto;
+
+void main() {
+    // Hacer una aplicación para una agenda del celular
+
+    Contacto miContacto = new Contacto();
+    miContacto.nombre = "Fulano";
+    miContacto.apellido = "De Tal";
+    miContacto.numeroCelular = "299155111111";
+    miContacto.email = "fulano@epet14.edu.ar";
+
+    Contacto otroContacto = new Contacto();
+
+    otroContacto.nombre = "Mengano";
+    otroContacto.apellido = "De aquel";
+    otroContacto.numeroCelular = "299155111112";
+    otroContacto.email = "mengano@epet14.edu.ar";
+
+
+    // Una clase "Estudiante" que tenga:
+    // Nombre
+    // Apellido
+    // Curso: int
+    // División: int
+    // Especialidad:
+
+    // En otro archivo con método main crear
+    // un objeto de la clase estudiante y darle
+    // valores a los atributos
+
+}
